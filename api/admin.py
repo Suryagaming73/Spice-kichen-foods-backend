f374d65570124f44303f8a4130768817825a85c6
Surya <cssurya2006@gmail.com>
2026-07-08 15:14:56 +0530

    Register models in Django Admin

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import UserProfile, Category, FoodItem, Order, Review, HotelSetting, ContactMessage
 
 # Register your models here.
+admin.site.register(UserProfile)
+admin.site.register(Category)
+admin.site.register(FoodItem)
+admin.site.register(Order)
+admin.site.register(Review)
+admin.site.register(HotelSetting)
+admin.site.register(ContactMessage)
